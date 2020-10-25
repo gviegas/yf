51f00e4804a620546c02a6ca1dfc81e9ccbeb743
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-10-25 14:57:03 -0300

    ShaderVK

---
+++ cg/src/vk/ShaderVK.cxx
@@ -0,0 +1,64 @@
+//
+// yf
+// ShaderVK.cxx
+//
+// Copyright © 2020 Gustavo C. Viegas.
+//
+
+#include <cassert>
+#include <fstream>
+#include <filesystem>
+#include <memory>
+
+#include "ShaderVK.h"
+#include "DeviceVK.h"
+
+using namespace CG_NS;
+using namespace std;
+
+ShaderVK::ShaderVK(Stage stage, wstring&& codeFile, wstring&& entryPoint)
+  : Shader(stage, move(codeFile), move(entryPoint)) {
+
+  ifstream ifs(filesystem::path{codeFile});
+  if (!ifs)
+    // TODO
+    throw runtime_error("Could not open file");
+
+  ifs.seekg(0, ios_base::end);
+  const auto sz = ifs.tellg();
+  if (sz == 0 || sz%4 != 0)
+    // TODO
+    throw runtime_error("Invalid file");
+
+  ifs.seekg(0);
+  auto buf = make_unique<char[]>(sz);
+  if (!ifs.read(buf.get(), sz))
+    // TODO
+    throw runtime_error("Failed to read data from file");
+
+  auto dev = DeviceVK::get().device();
+  CG_DEVPROCVK(dev, vkCreateShaderModule);
+
+  VkShaderModuleCreateInfo info;
+  info.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+  info.pNext = nullptr;
+  info.flags = 0;
+  info.codeSize = sz;
+  info.pCode = reinterpret_cast<uint32_t*>(buf.get());
+
+  auto res = vkCreateShaderModule(dev, &info, nullptr, &_module);
+  if (res != VK_SUCCESS)
+    // TODO
+    throw runtime_error("Failed to create shader module");
+}
+
+ShaderVK::~ShaderVK() {
+  // TODO: notify
+  auto dev = DeviceVK::get().device();
+  CG_DEVPROCVK(dev, vkDestroyShaderModule);
+  vkDestroyShaderModule(dev, _module, nullptr);
+}
+
+VkShaderModule ShaderVK::module() const {
+  return _module;
+}
---
+++ cg/src/vk/ShaderVK.h
@@ -0,0 +1,29 @@
+//
+// yf
+// ShaderVK.h
+//
+// Copyright © 2020 Gustavo C. Viegas.
+//
+
+#ifndef YF_CG_SHADERVK_H
+#define YF_CG_SHADERVK_H
+
+#include "yf/cg/Shader.h"
+#include "VK.h"
+
+CG_NS_BEGIN
+
+class ShaderVK final : public Shader {
+ public:
+  ShaderVK(Stage stage, std::wstring&& codeFile, std::wstring&& entryPoint);
+  ~ShaderVK();
+
+  VkShaderModule module() const;
+
+ private:
+  VkShaderModule _module = VK_NULL_HANDLE;
+};
+
+CG_NS_END
+
+#endif // YF_CG_SHADERVK_H

1fd4bd4ba124213e73fea53ed599c1c831cd68df
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-11-19 16:54:14 -0300

    WS Defs.h

---
+++ ws/include/Defs.h
@@ -0,0 +1,22 @@
+//
+// WS
+// Defs.h
+//
+// Copyright © 2020 Gustavo C. Viegas.
+//
+
+#ifndef YF_WS_DEFS_H
+#define YF_WS_DEFS_H
+
+#include "yf/Defs.h"
+#include "yf/Internal.h"
+
+#undef WS_NS
+#undef WS_NS_BEGIN
+#undef WS_NS_END
+
+#define WS_NS YF_NS::ws
+#define WS_NS_BEGIN namespace WS_NS {
+#deinfE WS_NS_END }
+
+#endif // YF_WS_DEFS_H

07bffeb6a40d7a0133e0794b3d0cc9d3ec8ec22b
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-11-21 19:39:13 -0300

    Window make()

--- ws/src/Window.cxx
+++ ws/src/Window.cxx
@@ -5,7 +5,21 @@
 // Copyright © 2020 Gustavo C. Viegas.
 //
 
-#include "Window.h"
+#if defined(__linux__) // TODO: add other unix systems here
+//# include "unix/WindowWL.h"
+# include "unix/WindowXCB.h"
+# define WS_WINDOW_T WindowXCB
+#elif defined(__APPLE__)
+# include "macos/WindowMAC.h"
+# define WS_WINDOW_T WindowMAC
+#elif defined(_WIN32)
+# include "win32/WindowW32.h"
+# define WS_WINDOW_T WindowW32
+#else
+# error "Invalid platform"
+#endif // defined(__linux__)
+
+//#include "Window.h"
 
 using namespace WS_NS;
 using namespace std;
@@ -14,8 +28,8 @@ Window::Ptr Window::make(uint32_t width,
                          uint32_t height,
                          const wstring& title,
                          CreationMask mask) {
-  // TODO
-  return nullptr;
+
+  return make_unique<WS_WINDOW_T>(width, height, title, mask);
 }
 
 Window::~Window() { }

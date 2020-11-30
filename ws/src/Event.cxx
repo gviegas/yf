bc60dc2f5d86a96eaa22c31cec525792ef0b4746
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-11-30 19:47:13 -0300

    WS Event

---
+++ ws/src/Event.cxx
@@ -0,0 +1,19 @@
+//
+// WS
+// Event.cxx
+//
+// Copyright © 2020 Gustavo C. Viegas.
+//
+
+#include <cassert>
+
+#include "Event.h"
+
+using namespace WS_NS;
+
+Event::~Event() { }
+
+Event& Event::get() {
+  // TODO
+  assert(false);
+}

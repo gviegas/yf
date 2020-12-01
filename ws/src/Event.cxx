0cc98ca40bdbb170a1ae28519628780462a2b2a9
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-01 16:57:42 -0300

    WS Event get()

--- ws/src/Event.cxx
+++ ws/src/Event.cxx
@@ -5,15 +5,26 @@
 // Copyright © 2020 Gustavo C. Viegas.
 //
 
-#include <cassert>
+#if defined(__linux__) // TODO: add other unix systems here
+//# include "unix/EventWL.h"
+# include "unix/EventXCB.h"
+# define WS_EVENT_T EventXCB
+#elif defined(__APPLE__)
+# include "macos/EventMAC.h"
+# define WS_EVENT_T EventMAC
+#elif defined(_WIN32)
+# include "win32/EventW32.h"
+# define WS_EVENT_T EventW32
+#else
+# error "Invalid platform"
+#endif // defined(__linux__)
 
-#include "Event.h"
+//#include "Event.h"
 
 using namespace WS_NS;
 
 Event::~Event() { }
 
 Event& Event::get() {
-  // TODO
-  assert(false);
+  return WS_EVENT_T::get();
 }

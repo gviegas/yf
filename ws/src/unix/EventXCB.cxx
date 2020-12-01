2a5b3befe2e4ad0690113e6292fedccfd5844381
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-01 12:01:42 -0300

    EventXCB setDelegate()

--- ws/src/unix/EventXCB.cxx
+++ ws/src/unix/EventXCB.cxx
@@ -22,16 +22,25 @@ void EventXCB::dispatch() {
 }
 
 void EventXCB::setDelegate(const WdDelegate& delegate) {
-  // TODO
-  assert(false);
+  wdDeleg_ = delegate;
+
+  mask_ = wdDeleg_.close  ? (mask_ | WdClose)  : (mask_ & ~WdClose);
+  mask_ = wdDeleg_.resize ? (mask_ | WdResize) : (mask_ & ~WdResize);
 }
 
 void EventXCB::setDelegate(const KbDelegate& delegate) {
-  // TODO
-  assert(false);
+  kbDeleg_ = delegate;
+
+  mask_ = kbDeleg_.enter ? (mask_ | KbEnter) : (mask_ & ~KbEnter);
+  mask_ = kbDeleg_.leave ? (mask_ | KbLeave) : (mask_ & ~KbLeave);
+  mask_ = kbDeleg_.key   ? (mask_ | KbKey)   : (mask_ & ~KbKey);
 }
 
 void EventXCB::setDelegate(const PtDelegate& delegate) {
-  // TODO
-  assert(false);
+  ptDeleg_ = delegate;
+
+  mask_ = ptDeleg_.enter  ? (mask_ | PtEnter)  : (mask_ & ~PtEnter);
+  mask_ = ptDeleg_.leave  ? (mask_ | PtLeave)  : (mask_ & ~PtLeave);
+  mask_ = ptDeleg_.motion ? (mask_ | PtMotion) : (mask_ & ~PtMotion);
+  mask_ = ptDeleg_.button ? (mask_ | PtButton) : (mask_ & ~PtButton);
 }

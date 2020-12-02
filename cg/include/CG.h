8b843eec194ec7ea13b5b98fc5a8d003c5860731
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-02 14:20:53 -0300

    CG.h

---
+++ cg/include/CG.h
@@ -0,0 +1,24 @@
+//
+// CG
+// CG.h
+//
+// Copyright © 2020 Gustavo C. Viegas.
+//
+
+#ifndef YF_CG_H
+#define YF_CG_H
+
+#include <yf/cg/Defs.h>
+#include <yf/cg/Types.h>
+#include <yf/cg/Device.h>
+#include <yf/cg/Queue.h>
+#include <yf/cg/Buffer.h>
+#include <yf/cg/Image.h>
+#include <yf/cg/Pass.h>
+#include <yf/cg/Shader.h>
+#include <yf/cg/DcTable.h>
+#include <yf/cg/State.h>
+#include <yf/cg/Encoder.h>
+#include <yf/cg/Wsi.h>
+
+#endif // YF_CG_H

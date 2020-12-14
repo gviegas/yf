cb0665e72de940b9671cf1e01a92ad8e1df97b17
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-14 13:20:56 -0300

    WS event instance now provided by platform's 'getEvent()'

--- ws/src/Event.cxx
+++ ws/src/Event.cxx
@@ -5,26 +5,17 @@
 // Copyright © 2020 Gustavo C. Viegas.
 //
 
-#if defined(__linux__) // TODO: add other unix systems here
-//# include "unix/EventWL.h"
-# include "unix/EventXCB.h"
-# define WS_EVENT_T EventXCB
-#elif defined(__APPLE__)
-# include "macos/EventMAC.h"
-# define WS_EVENT_T EventMAC
-#elif defined(_WIN32)
-# include "win32/EventW32.h"
-# define WS_EVENT_T EventW32
-#else
-# error "Invalid platform"
-#endif // defined(__linux__)
+#include "Event.h"
 
-//#include "Event.h"
-
-using namespace WS_NS;
+WS_NS_BEGIN
 
 Event::~Event() { }
 
+// `Platform` provides the implementation.
+Event& getEvent();
+
 Event& Event::get() {
-  return WS_EVENT_T::get();
+  return getEvent();
 }
+
+WS_NS_END

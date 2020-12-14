95abb723a4087c1f4e3cc9c659810e50536367aa
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-14 13:02:24 -0300

    WS window imp. now provided by platform's 'makeWindow()'

--- ws/src/Window.cxx
+++ ws/src/Window.cxx
@@ -5,31 +5,26 @@
 // Copyright © 2020 Gustavo C. Viegas.
 //
 
-#if defined(__linux__) // TODO: add other unix systems here
-//# include "unix/WindowWL.h"
-# include "unix/WindowXCB.h"
-# define WS_WINDOW_T WindowXCB
-#elif defined(__APPLE__)
-# include "macos/WindowMAC.h"
-# define WS_WINDOW_T WindowMAC
-#elif defined(_WIN32)
-# include "win32/WindowW32.h"
-# define WS_WINDOW_T WindowW32
-#else
-# error "Invalid platform"
-#endif // defined(__linux__)
+#include "Window.h"
 
-//#include "Window.h"
-
-using namespace WS_NS;
 using namespace std;
+
+WS_NS_BEGIN
+
+// `Platform` provides the implementation.
+Window::Ptr makeWindow(uint32_t width,
+                       uint32_t height,
+                       const wstring& title,
+                       Window::CreationMask mask);
 
 Window::Ptr Window::make(uint32_t width,
                          uint32_t height,
                          const wstring& title,
                          CreationMask mask) {
 
-  return make_unique<WS_WINDOW_T>(width, height, title, mask);
+  return makeWindow(width, height, title, mask);
 }
 
 Window::~Window() { }
+
+WS_NS_END

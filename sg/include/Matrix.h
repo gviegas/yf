1d92bceac80b6331709a1aa2b83f3ea82702a27a
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2020-12-17 01:43:28 -0300

    SG Matrix inversion

--- sg/include/Matrix.h
+++ sg/include/Matrix.h
@@ -163,6 +163,81 @@ constexpr Matrix<T, sqN, sqN> transpose(const Matrix<T, sqN, sqN>& mat) {
   return res;
 }
 
+template<class T>
+constexpr Matrix<T, 2, 2> invert(const Matrix<T, 2, 2>& mat) {
+  static_assert(std::is_floating_point<T>(),
+                "Matrix inversion requires a floating point type");
+
+  const T idet = 1.0 / (mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]);
+  return {{mat[1][1]*idet, mat[0][1]*idet}, {-mat[1][0]*idet, mat[0][0]*idet}};
+}
+
+template<class T>
+constexpr Matrix<T, 3, 3> invert(const Matrix<T, 3, 3>& mat) {
+  static_assert(std::is_floating_point<T>(),
+                "Matrix inversion requires a floating point type");
+
+  Matrix<T, 3, 3> res;
+
+  const T s0 = mat[1][1] * mat[2][2] - mat[1][2] * mat[2][1];
+  const T s1 = mat[1][0] * mat[2][2] - mat[1][2] * mat[2][0];
+  const T s2 = mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0];
+  const T idet = 1.0 / (mat[0][0]*s0 - mat[0][1]*s1 + mat[0][2]*s2);
+
+  res[0][0] = +s0 * idet;
+  res[0][1] = -(mat[0][1] * mat[2][2] - mat[0][2] * mat[2][1]) * idet;
+  res[0][2] = +(mat[0][1] * mat[1][2] - mat[0][2] * mat[1][1]) * idet;
+  res[1][0] = -s1 * idet;
+  res[1][1] = +(mat[0][0] * mat[2][2] - mat[0][2] * mat[2][0]) * idet;
+  res[1][2] = -(mat[0][0] * mat[1][2] - mat[0][2] * mat[1][0]) * idet;
+  res[2][0] = +s2 * idet;
+  res[2][1] = -(mat[0][0] * mat[2][1] - mat[0][1] * mat[2][0]) * idet;
+  res[2][2] = +(mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0]) * idet;
+
+  return res;
+}
+
+template<class T>
+constexpr Matrix<T, 4, 4> invert(const Matrix<T, 4, 4>& mat) {
+  static_assert(std::is_floating_point<T>(),
+                "Matrix inversion requires a floating point type");
+
+  Matrix<T, 4, 4> res;
+
+  const T s0 = mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+  const T s1 = mat[0][0] * mat[1][2] - mat[0][2] * mat[1][0];
+  const T s2 = mat[0][0] * mat[1][3] - mat[0][3] * mat[1][0];
+  const T s3 = mat[0][1] * mat[1][2] - mat[0][2] * mat[1][1];
+  const T s4 = mat[0][1] * mat[1][3] - mat[0][3] * mat[1][1];
+  const T s5 = mat[0][2] * mat[1][3] - mat[0][3] * mat[1][2];
+  const T c0 = mat[2][0] * mat[3][1] - mat[2][1] * mat[3][0];
+  const T c1 = mat[2][0] * mat[3][2] - mat[2][2] * mat[3][0];
+  const T c2 = mat[2][0] * mat[3][3] - mat[2][3] * mat[3][0];
+  const T c3 = mat[2][1] * mat[3][2] - mat[2][2] * mat[3][1];
+  const T c4 = mat[2][1] * mat[3][3] - mat[2][3] * mat[3][1];
+  const T c5 = mat[2][2] * mat[3][3] - mat[2][3] * mat[3][2];
+  const T idet = 1.0 / (s0*c5 - s1*c4 + s2*c3 + s3*c2 - s4*c1 + s5*c0);
+
+  res[0][0] = (+c5 * mat[1][1] - c4 * mat[1][2] + c3 * mat[1][3]) * idet;
+  res[0][1] = (-c5 * mat[0][1] + c4 * mat[0][2] - c3 * mat[0][3]) * idet;
+  res[0][2] = (+s5 * mat[3][1] - s4 * mat[3][2] + s3 * mat[3][3]) * idet;
+  res[0][3] = (-s5 * mat[2][1] + s4 * mat[2][2] - s3 * mat[2][3]) * idet;
+  res[1][0] = (-c5 * mat[1][0] + c2 * mat[1][2] - c1 * mat[1][3]) * idet;
+  res[1][1] = (+c5 * mat[0][0] - c2 * mat[0][2] + c1 * mat[0][3]) * idet;
+  res[1][2] = (-s5 * mat[3][0] + s2 * mat[3][2] - s1 * mat[3][3]) * idet;
+  res[1][3] = (+s5 * mat[2][0] - s2 * mat[2][2] + s1 * mat[2][3]) * idet;
+  res[2][0] = (+c4 * mat[1][0] - c2 * mat[1][1] + c0 * mat[1][3]) * idet;
+  res[2][1] = (-c4 * mat[0][0] + c2 * mat[0][1] - c0 * mat[0][3]) * idet;
+  res[2][2] = (+s4 * mat[3][0] - s2 * mat[3][1] + s0 * mat[3][3]) * idet;
+  res[2][3] = (-s4 * mat[2][0] + s2 * mat[2][1] - s0 * mat[2][3]) * idet;
+  res[3][0] = (-c3 * mat[1][0] + c1 * mat[1][1] - c0 * mat[1][2]) * idet;
+  res[3][1] = (+c3 * mat[0][0] - c1 * mat[0][1] + c0 * mat[0][2]) * idet;
+  res[3][2] = (-s3 * mat[3][0] + s1 * mat[3][1] - s0 * mat[3][2]) * idet;
+  res[3][3] = (+s3 * mat[2][0] - s1 * mat[2][1] + s0 * mat[2][2]) * idet;
+
+  return res;
+}
+
 using Mat2i   = Matrix<int32_t, 2, 2>;
 using Mat2x3i = Matrix<int32_t, 2, 3>;
 using Mat2x4i = Matrix<int32_t, 2, 4>;

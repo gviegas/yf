a337102f6ee101bb77012a8000486ae894ba96c3
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-03-14 00:40:15 -0300

    [WS] Event now declares a single dispatch() function

--- ws/include/Event.h
+++ ws/include/Event.h
@@ -2,47 +2,19 @@
 // WS
 // Event.h
 //
-// Copyright © 2020 Gustavo C. Viegas.
+// Copyright © 2020-2021 Gustavo C. Viegas.
 //
 
 #ifndef YF_WS_EVENT_H
 #define YF_WS_EVENT_H
 
-#include <memory>
-
 #include "yf/ws/Defs.h"
 
 WS_NS_BEGIN
 
-struct WdDelegate;
-struct KbDelegate;
-struct PtDelegate;
-
-/// Event manager.
+/// Dispatches events.
 ///
-class Event {
- public:
-  virtual ~Event();
-
-  /// Gets the event instance.
-  ///
-  static Event& get();
-
-  /// Dispatches events.
-  ///
-  virtual void dispatch() = 0;
-
-  /// Sets event delegates.
-  ///
-  void setDelegate(const WdDelegate& delegate);
-  void setDelegate(const KbDelegate& delegate);
-  void setDelegate(const PtDelegate& delegate);
-
- protected:
-  Event();
-  class Impl;
-  std::unique_ptr<Impl> impl_;
-};
+void dispatch();
 
 WS_NS_END
 

efce44b7e2d9aeae624764b2862fec08e7b120e4
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-03-14 12:13:28 -0300

    [WS] EventXCB updated

--- ws/src/unix/EventXCB.h
+++ ws/src/unix/EventXCB.h
@@ -2,28 +2,19 @@
 // WS
 // EventXCB.h
 //
-// Copyright © 2020 Gustavo C. Viegas.
+// Copyright © 2020-2021 Gustavo C. Viegas.
 //
 
 #ifndef YF_WS_EVENTXCB_H
 #define YF_WS_EVENTXCB_H
 
 #include "Event.h"
-#include "Window.h"
-#include "Keyboard.h"
-#include "Pointer.h"
 
 WS_NS_BEGIN
 
-class EventXCB final : public Event {
- public:
-  ~EventXCB() = default;
-  static EventXCB& get();
-  void dispatch();
-
- private:
-  EventXCB() = default;
-};
+/// Dispatches events.
+///
+void dispatchXCB();
 
 WS_NS_END
 

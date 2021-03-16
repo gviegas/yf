726f45d5a6e2204ed8cd8a0352b8d9424f048e1c
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-03-16 00:22:03 -0300

    [WS] minor interface changes

--- ws/include/WS.h
+++ ws/include/WS.h
@@ -2,7 +2,7 @@
 // WS
 // WS.h
 //
-// Copyright © 2020 Gustavo C. Viegas.
+// Copyright © 2020-2021 Gustavo C. Viegas.
 //
 
 #ifndef YF_WS_H
@@ -10,11 +10,11 @@
 
 /// WS interface.
 ///
-#include <yf/ws/Defs.h>
-#include <yf/ws/Event.h>
-#include <yf/ws/Keyboard.h>
-#include <yf/ws/Platform.h>
-#include <yf/ws/Pointer.h>
-#include <yf/ws/Window.h>
+#include "yf/ws/Defs.h"
+#include "yf/ws/Event.h"
+#include "yf/ws/Keyboard.h"
+#include "yf/ws/Platform.h"
+#include "yf/ws/Pointer.h"
+#include "yf/ws/Window.h"
 
 #endif // YF_WS_H

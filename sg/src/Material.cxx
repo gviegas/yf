4b8a69772dc686fbac165310cd60d32539683fb0
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-04-16 11:06:33 -0300

    [SG] Material getters

--- sg/src/Material.cxx
+++ sg/src/Material.cxx
@@ -17,9 +17,26 @@ class Material::Impl {
     : pbrmr_(pbrmr), normal_(normal), occlusion_(occlusion),
       emissive_(emissive) { }
 
+  Impl() { }
   Impl(const Impl&) = delete;
   Impl& operator=(const Impl&) = delete;
   ~Impl() = default;
+
+  const Pbrmr& pbrmr() const {
+    return pbrmr_;
+  }
+
+  const Normal& normal() const {
+    return normal_;
+  }
+
+  const Occlusion& occlusion() const {
+    return occlusion_;
+  }
+
+  const Emissive& emissive() const {
+    return emissive_;
+  }
 
  private:
   Pbrmr pbrmr_{};
@@ -27,3 +44,23 @@ class Material::Impl {
   Occlusion occlusion_{};
   Emissive emissive_{};
 };
+
+Material::Material() : impl_(make_unique<Impl>()) { }
+
+Material::~Material() { }
+
+const Material::Pbrmr& Material::pbrmr() const {
+  return impl_->pbrmr();
+}
+
+const Material::Normal& Material::normal() const {
+  return impl_->normal();
+}
+
+const Material::Occlusion& Material::occlusion() const {
+  return impl_->occlusion();
+}
+
+const Material::Emissive& Material::emissive() const {
+  return impl_->emissive();
+}

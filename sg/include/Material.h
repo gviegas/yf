f2dcde9581786236d18bcb7dc3ca44d3f97d9e8f
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-04-16 01:11:57 -0300

    [SG] Material

---
+++ sg/include/Material.h
@@ -0,0 +1,77 @@
+//
+// SG
+// Material.h
+//
+// Copyright © 2021 Gustavo C. Viegas.
+//
+
+#ifndef YF_SG_MATERIAL_H
+#define YF_SG_MATERIAL_H
+
+#include "yf/sg/Defs.h"
+#include "yf/sg/Vector.h"
+
+SG_NS_BEGIN
+
+class Texture;
+
+/// Material.
+///
+class Material {
+ public:
+  /// PBR metallic-roughness.
+  ///
+  struct Pbrmr {
+    Texture* colorTex = nullptr;
+    Vec4f colorFac{1.0f, 1.0f, 1.0f, 1.0f};
+    Texture* metalRoughTex = nullptr;
+    float metalness = 1.0f;
+    float roughness = 1.0f;
+  };
+
+  /// Normal map.
+  ///
+  struct Normal {
+    Texture* texture = nullptr;
+    float scale = 1.0f;
+  };
+
+  /// Occlusion map.
+  ///
+  struct Occlusion {
+    Texture* texture = nullptr;
+    float strength = 1.0f;
+  };
+
+  /// Emissive map.
+  ///
+  struct Emissive {
+    Texture* texture = nullptr;
+    Vec3f factor{};
+  };
+
+  Material();
+  ~Material();
+
+  /// Getters.
+  ///
+  const Pbrmr& pbrmr() const;
+  const Normal& normal() const;
+  const Occlusion& occlusion() const;
+  const Emissive& emissive() const;
+
+  /// Setters.
+  ///
+  void setPbrmr(const Pbrmr&);
+  void setNormal(const Normal&);
+  void setOcclusion(const Occlusion&);
+  void setEmissive(const Emissive&);
+
+ private:
+  class Impl;
+  std::unique_ptr<Impl> impl_;
+};
+
+SG_NS_END
+
+#endif // YF_SG_MATERIAL_H

96b2cb1e217ea2aed06d92b1c0e2c62f65ef9726
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-04-16 12:27:40 -0300

    [SG] MaterialTest additions

--- sg/test/MaterialTest.cxx
+++ sg/test/MaterialTest.cxx
@@ -22,7 +22,44 @@ struct MaterialTest : Test {
   Assertions run(const vector<string>&) {
     Assertions a;
 
-    // TODO
+    auto print = [](const Material& ml) {
+      const auto& pbrmr = ml.pbrmr();
+      const auto& normal = ml.normal();
+      const auto& occlusion = ml.occlusion();
+      const auto& emissive = ml.emissive();
+      wcout << "\nMaterial:"
+            << "\n Pbrmr:"
+            << "\n  colorTex: " << pbrmr.colorTex
+            << "\n  colorFac: [" << pbrmr.colorFac[0] << ", "
+                                 << pbrmr.colorFac[1] << ", "
+                                 << pbrmr.colorFac[2] << ", "
+                                 << pbrmr.colorFac[3] << "]"
+            << "\n  metalRoughTex: " << pbrmr.metalRoughTex
+            << "\n  metalness: " << pbrmr.metalness
+            << "\n  roughness: " << pbrmr.roughness
+            << "\n Normal:"
+            << "\n  texture: " << normal.texture
+            << "\n  scale: " << normal.scale
+            << "\n Occlusion:"
+            << "\n  texture: " << occlusion.texture
+            << "\n  strength: " << occlusion.strength
+            << "\n Emissive:"
+            << "\n  texture: " << emissive.texture
+            << "\n  factor: [" << emissive.factor[0] << ", "
+                               << emissive.factor[1] << ", "
+                               << emissive.factor[2] << "]"
+            << endl;
+    };
+
+    Material ml1{};
+    print(ml1);
+
+    Material::Occlusion occlusion{nullptr, 0.5f};
+    Material::Emissive emissive{nullptr, {0.1f, 0.2f, 0.3f}};
+
+    Material ml2{{}, {}, occlusion, emissive};
+    print(ml2);
+
     return a;
   }
 };

743dbf4d604ce278c0044aba1084b46e12cd100c
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-04-17 16:11:06 -0300

    [SG] SceneTest additions

--- sg/test/SceneTest.cxx
+++ sg/test/SceneTest.cxx
@@ -5,8 +5,12 @@
 // Copyright © 2020-2021 Gustavo C. Viegas.
 //
 
+#include <iostream>
+#include <algorithm>
+
 #include "Test.h"
 #include "Scene.h"
+#include "Camera.h"
 
 using namespace TEST_NS;
 using namespace SG_NS;
@@ -18,9 +22,29 @@ struct SceneTest : Test {
   SceneTest() : Test(L"Scene") { }
 
   Assertions run(const vector<string>&) {
-    // TODO
+    Assertions a;
+
     Scene scn;
-    return {{L"Scene()", scn.isRoot() && scn.isLeaf()}};
+    a.push_back({L"Scene()", scn.isRoot() && scn.isLeaf()});
+
+    const auto& xform = scn.camera().transform();
+    auto prec = wcout.precision(3);
+    wcout << "\ncamera().transform():\n";
+    for (size_t i = 0; i < xform.rows(); ++i) {
+      wcout << "\n ";
+      for (size_t j = 0; j < xform.columns(); ++j)
+        wcout << xform[j][i] << "\t";
+    }
+    wcout << endl;
+    wcout.precision(prec);
+    a.push_back({L"camera()", true});
+
+    array<float, 4> color{1.0f, 1.0f, 1.0f, 1.0f};
+    scn.color() = color;
+    a.push_back({L"color()", equal(color.begin(), color.end(),
+                                   scn.color().begin())});
+
+    return a;
   }
 };
 

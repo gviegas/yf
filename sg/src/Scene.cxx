544d04a069059dc9b8d0e26363163d07d6db307c
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-04-17 14:56:56 -0300

    [SG] Scene camera() completed

--- sg/src/Scene.cxx
+++ sg/src/Scene.cxx
@@ -2,10 +2,11 @@
 // SG
 // Scene.cxx
 //
-// Copyright © 2020 Gustavo C. Viegas.
+// Copyright © 2020-2021 Gustavo C. Viegas.
 //
 
 #include "Scene.h"
+#include "Camera.h"
 
 using namespace SG_NS;
 using namespace std;
@@ -13,8 +14,14 @@ using namespace std;
 // TODO
 
 class Scene::Impl {
+ public:
+  Camera camera_{{20.0f, 20.0f, 20.0f}, {}, 1.0f};
 };
 
 Scene::Scene() : impl_(make_unique<Impl>()) { }
 
 Scene::~Scene() { }
+
+Camera& Scene::camera() {
+  return impl_->camera_;
+}

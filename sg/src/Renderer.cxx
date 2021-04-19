44bae0002a088e5627dd4956fd7f24da1322d5e5
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-04-19 11:16:13 -0300

    [SG] Renderer processGraph() additions

--- sg/src/Renderer.cxx
+++ sg/src/Renderer.cxx
@@ -5,6 +5,7 @@
 // Copyright © 2021 Gustavo C. Viegas.
 //
 
+#include <algorithm>
 #include <typeinfo>
 #include <stdexcept>
 
@@ -55,4 +56,18 @@ void Renderer::processGraph(Scene& scene) {
         it->second.push_back(&mdl);
     }
   }, true);
+
+  // TODO: instanced rendering
+  if (models_.empty()) {
+    mdlShader_.reset();
+    mdlTable_.reset();
+    mdlState_.reset();
+  } else {
+    // XXX
+    if (any_of(models_.begin(), models_.end(),
+               [](const auto& kv) { return kv.second.size() > 1; }))
+      throw runtime_error("Instanced rendering of models unimplemented");
+
+    // TODO...
+  }
 }

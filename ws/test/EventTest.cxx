d0a6af5ad761420d2d336e30f172ff86d54a67af
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-04-24 21:33:19 -0300

    [WS] EventTest updated

--- ws/test/EventTest.cxx
+++ ws/test/EventTest.cxx
@@ -28,34 +28,28 @@ struct EventTest : Test {
 
     KeyCode key = KeyCodeUnknown;
 
-    WdDelegate wdDeleg{
-      [](Window* win) { wcout << "wd close: " << win << endl; },
-      [](Window* win, uint32_t w, uint32_t h) {
-        wcout << "wd resize: " << win << ", " << w << ", " << h << endl; }
-    };
-    setDelegate(wdDeleg);
+    onWdClose([](Window* win) { wcout << "wd close: " << win << endl; });
+    onWdResize([](Window* win, uint32_t w, uint32_t h) {
+        wcout << "wd resize: " << win << ", " << w << ", " << h << endl;
+    });
 
-    KbDelegate kbDeleg{
-      [](Window* win) { wcout << "kb enter: " << win << endl; },
-      [](Window* win) { wcout << "kb leave: " << win << endl; },
-      [&key](KeyCode c, KeyState s, KeyModMask m) {
-        wcout << "kb key: " << c << ", " << s << ", " << hex << m << dec
-              << endl;
-        key = c; }
-    };
-    setDelegate(kbDeleg);
+    onKbEnter([](Window* win) { wcout << "kb enter: " << win << endl; });
+    onKbLeave([](Window* win) { wcout << "kb leave: " << win << endl; });
+    onKbKey([&key](KeyCode c, KeyState s, KeyModMask m) {
+      wcout << "kb key: " << c << ", " << s << ", " << hex << m << dec << endl;
+      key = c;
+    });
 
-    PtDelegate ptDeleg{
-      [](Window* win, int32_t x, int32_t y) {
-        wcout << "pt enter: " << win << ", " << x << ", " << y << endl; },
-      [](Window* win) { wcout << "pt leave: " << win << endl; },
-      [](int32_t x, int32_t y) {
-        wcout << "pt motion: " << x << ", " << y << endl; },
-      [](Button b, ButtonState s, int32_t x, int32_t y) {
-        wcout << "pt button: " << b << ", " << s << "," << x << ", " << y
-              << endl; }
-    };
-    setDelegate(ptDeleg);
+    onPtEnter([](Window* win, int32_t x, int32_t y) {
+      wcout << "pt enter: " << win << ", " << x << ", " << y << endl;
+    });
+    onPtLeave([](Window* win) { wcout << "pt leave: " << win << endl; });
+    onPtMotion([](int32_t x, int32_t y) {
+      wcout << "pt motion: " << x << ", " << y << endl;
+    });
+    onPtButton([](Button b, ButtonState s, int32_t x, int32_t y) {
+      wcout << "pt button: " << b << ", " << s << "," << x << ", " << y << endl;
+    });
 
     auto win1 = createWindow(300, 200, L"Window 1");
     auto win2 = createWindow(100, 300, L"Window 2");
@@ -67,6 +61,13 @@ struct EventTest : Test {
 
       if (key == KeyCodeEsc)
         break;
+
+      if (key == KeyCode1)
+        onWdClose({});
+      else if (key == KeyCode2)
+        onWdClose([](Window* win) {
+          wcout << "new wd close: " << win << endl;
+        });
     }
 
     a.push_back({L"setDelegate(WdDelegate)", true});

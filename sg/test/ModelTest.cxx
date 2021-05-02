131e595e262185b92e1bcd3ee9eaaf53d2ea2616
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-05-02 19:53:07 -0300

    [SG] Model skin completed

--- sg/test/ModelTest.cxx
+++ sg/test/ModelTest.cxx
@@ -11,6 +11,7 @@
 #include "Model.h"
 #include "Mesh.h"
 #include "Material.h"
+#include "Skin.h"
 
 using namespace TEST_NS;
 using namespace SG_NS;
@@ -26,39 +27,49 @@ struct ModelTest : Test {
 
     Model mdl1;
 
-    a.push_back({L"Model()", !mdl1.mesh() && !mdl1.material()});
-
     Mesh mesh{Mesh::FileType::Gltf, L"tmp/cube.gltf"};
-    Model mdl2{mesh};
+    Model mdl2;
+    mdl2.setMesh(&mesh);
 
-    a.push_back({L"Model(mesh)", mdl2.mesh() == &mesh && !mdl2.material()});
+    a.push_back({L"Model()", !mdl1.mesh() && !mdl1.material() &&
+                             mdl2.mesh() == &mesh && !mdl2.material()});
 
     Material material;
-    Model mdl3{mesh, material};
+    Skin skin({Mat4f::identity()}, {});
+    Model mdl3{mesh, material, skin};
 
-    a.push_back({L"Model(mesh, material)", mdl3.mesh() == &mesh &&
-                                           mdl3.material() == &material});
+    a.push_back({L"Model(mesh, material, skin)",
+                 mdl3.mesh() == &mesh && mdl3.material() == &material &&
+                 mdl3.skin() == &skin});
 
     Model mdl4{mdl3};
     a.push_back({L"Model(other)", mdl4.mesh() == &mesh &&
                                   mdl4.material() == &material &&
+                                  mdl4.skin() == &skin &&
                                   mdl4.mesh() == mdl3.mesh() &&
-                                  mdl4.material() == mdl3.material()});
+                                  mdl4.material() == mdl3.material() &&
+                                  mdl4.skin() == mdl3.skin()});
 
     Model mdl5 = mdl3;
     a.push_back({L"=", mdl5.mesh() == &mesh && mdl5.material() == &material &&
-                       mdl5.mesh() == mdl3.mesh() &&
-                       mdl5.material() == mdl3.material()});
+                       mdl5.skin() == &skin && mdl5.mesh() == mdl3.mesh() &&
+                       mdl5.material() == mdl3.material() &&
+                       mdl5.skin() == mdl3.skin()});
 
     mdl1.setMesh(&mesh);
     mdl1.setMaterial(&material);
+    mdl1.setSkin(&skin);
     mdl2.setMaterial(nullptr);
     mdl3.setMesh(nullptr);
+    mdl4.setSkin(nullptr);
 
     a.push_back({L"set*()", mdl1.mesh() == &mesh &&
                             mdl1.material() == &material &&
-                            mdl2.mesh() == &mesh  && !mdl2.material() &&
-                            !mdl3.mesh() && mdl3.material() == &material});
+                            mdl1.skin() == &skin && mdl2.mesh() == &mesh &&
+                            !mdl2.material() && !mdl2.skin() &&
+                            !mdl3.mesh() && mdl3.material() == &material &&
+                            mdl3.skin() == &skin && mdl4.mesh() == &mesh &&
+                            mdl4.material() == &material && !mdl4.skin()});
 
     return a;
   }

d76da789e8ebcf82c10f494eead91a4d19899b60
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-07-30 12:46:27 -0300

    [SG] AnimationTest updated

--- sg/test/AnimationTest.cxx
+++ sg/test/AnimationTest.cxx
@@ -79,7 +79,8 @@ struct AnimationTest : InteractiveTest {
   void fromFile() {
     //Collection coll(L"tmp/animation.gltf");
     //Collection coll(L"tmp/animation2.gltf");
-    Collection coll(L"tmp/animation3.gltf");
+    //Collection coll(L"tmp/animation3.gltf");
+    Collection coll(L"tmp/animation3.glb");
 
     auto printMatrix = [](const Mat4f& mat) {
       for (size_t i = 0; i < mat.rows(); ++i) {

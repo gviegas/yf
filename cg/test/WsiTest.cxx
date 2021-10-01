0703985b0cc493428d15677c2968f9f67b63172f
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-10-01 15:49:54 -0300

    [CG] WsiTest added operator[], begin() and end() tests

--- cg/test/WsiTest.cxx
+++ cg/test/WsiTest.cxx
@@ -10,6 +10,7 @@
 
 #include "Test.h"
 #include "Wsi.h"
+#include "Device.h"
 
 using namespace TEST_NS;
 using namespace CG_NS;
@@ -23,9 +24,18 @@ struct WsiTest : Test {
   Assertions run(const vector<string>&) {
     class Wsi_ : public Wsi {
       WS_NS::Window* window_;
-      vector<Image*> images_{3, nullptr};
+      vector<Image*> images_;
      public:
-      Wsi_(WS_NS::Window* window) : window_(window) { }
+      Wsi_(WS_NS::Window* window) : window_(window), images_() {
+        static auto i1 = device().image(PxFormatR8Unorm, {16}, 1, 1, Samples1);
+        static auto i2 = device().image(PxFormatR8Unorm, {16}, 2, 1, Samples1);
+        static auto i3 = device().image(PxFormatR8Unorm, {16}, 3, 1, Samples1);
+        static auto i4 = device().image(PxFormatR8Unorm, {16}, 4, 1, Samples1);
+        images_.push_back(i1.get());
+        images_.push_back(i3.get());
+        images_.push_back(i4.get());
+        images_.push_back(i2.get());
+      }
       Image* operator[](Index index) { return images_[index]; }
       const Image* operator[](Index index) const { return images_[index]; }
       Image* const* begin() { return images_.data(); }
@@ -51,6 +61,18 @@ struct WsiTest : Test {
     this_thread::sleep_for(chrono::milliseconds(500));
 
     a.push_back({L"Wsi(...)", &wsi.window() == win.get()});
+
+    bool chk = true;
+    Wsi::Index ix = 0;
+    const uint32_t lays[]{1, 3, 4, 2};
+    for (auto& img : wsi) {
+      if (img != wsi[ix] || img->layers() != lays[ix++]) {
+        chk = false;
+        break;
+      }
+    }
+
+    a.push_back({L"operator[], begin(), end()", chk});
 
     return a;
   }

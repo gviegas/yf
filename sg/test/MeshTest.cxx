86d43df388515b46fc7f0e91e54f2080e3737c2c
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-10-09 12:46:38 -0300

    [SG] MeshTest corrected for changes in Mesh

--- sg/test/MeshTest.cxx
+++ sg/test/MeshTest.cxx
@@ -23,37 +23,38 @@ struct MeshTest : Test {
 
     auto print = [](Mesh* mesh = nullptr) {
       if (!mesh) {
-        wcout << "\nSegments: " << Mesh::Impl::segments_.size() << "\n";
-        for (const auto& s : Mesh::Impl::segments_)
+        wcout << "\nSegments: " << Primitive::Impl::segments_.size() << "\n";
+        for (const auto& s : Primitive::Impl::segments_)
           wcout << " [" << s.offset << ":" << s.size << "]\n";
       } else {
-        auto& impl = mesh->impl();
-        wcout << "\nMesh Impl\n"
-              << " Primitives: " << impl.primitiveCount() << "\n";
-        for (uint32_t i = 0; i < impl.primitiveCount(); i++) {
+        wcout << "\nMesh\n"
+              << " Primitives: " << mesh->primitiveCount() << "\n";
+        for (uint32_t i = 0; i < mesh->primitiveCount(); i++) {
+          const auto& prim = mesh->primitive(i);
           wcout << "  [" << i << "] ";
-          switch (impl.topology(i)) {
+          switch (prim.topology()) {
           case CG_NS::TopologyPoint:
-            wcout << "Point\n";
+            wcout << "Point";
             break;
           case CG_NS::TopologyLine:
-            wcout << "Line\n";
+            wcout << "Line";
             break;
           case CG_NS::TopologyTriangle:
-            wcout << "Triangle\n";
+            wcout << "Triangle";
             break;
           case CG_NS::TopologyLnStrip:
-            wcout << "Line Strip\n";
+            wcout << "Line Strip";
             break;
           case CG_NS::TopologyTriStrip:
-            wcout << "Triangle Strip\n";
+            wcout << "Triangle Strip";
             break;
           case CG_NS::TopologyTriFan:
-            wcout << "Triangle Fan\n";
+            wcout << "Triangle Fan";
             break;
           default:
-            wcout << "???\n";
+            wcout << "???";
           }
+          wcout << ", " << hex << prim.dataMask() << dec << "h\n";
         }
       }
     };
@@ -63,25 +64,23 @@ struct MeshTest : Test {
     bool countChk = true;
     bool topChk = true;
     bool bindChk = true;
-    bool ibufChk = true;
 
-    auto& buf = Mesh::Impl::buffer_;
-    auto& segs = Mesh::Impl::segments_;
+    auto& buf = Primitive::Impl::buffer_;
+    auto& segs = Primitive::Impl::segments_;
 
-    a.push_back({L"Mesh::Impl::buffer_", buf != nullptr});
-    a.push_back({L"Mesh::Impl::segments_", segs.size() == 1 &&
-                                           segs.front().offset == 0 &&
-                                           segs.front().size == buf->size()});
+    a.push_back({L"Primitive::Impl::buffer_", buf != nullptr});
+    a.push_back({L"Primitive::Impl::segments_",
+                 segs.size() == 1 && segs.front().offset == 0 &&
+                 segs.front().size == buf->size()});
 
-    wcout << "\nBuffer: " << Mesh::Impl::buffer_->size() << endl;
+    wcout << "\nBuffer: " << Primitive::Impl::buffer_->size() << endl;
     print();
 
     Mesh::Data data;
     data.data.push_back(make_unique<char[]>(2048));
     data.primitives.push_back({});
     auto& prim = data.primitives.back();
-    prim.vxAccessors.emplace(VxTypePosition,
-                             Mesh::Data::Accessor{0, 0, 24, 12});
+    prim.accessors.push_back({VxDataPosition, 0, 0, 24, 12});
 
     Mesh m1(data);
     print(&m1);
@@ -94,14 +93,13 @@ struct MeshTest : Test {
           segs.front().size != buf->size() - (24*12))
         ctorChk = false;
     }
-    if (m1.impl().primitiveCount() != 1)
+    if (m1.primitiveCount() != 1)
       countChk = false;
-    if (m1.impl().topology(0) != CG_NS::TopologyTriangle)
+    if (m1[0].topology() != CG_NS::TopologyTriangle)
       topChk = false;
-    if (!m1.impl().canBind(VxTypePosition) || m1.impl().canBind(VxTypeNormal))
+    if (!(m1[0].dataMask() & VxDataPosition) ||
+        m1[0].dataMask() & VxDataNormal)
       bindChk = false;
-    if (m1.impl().isIndexed())
-      ibufChk = false;
 
     Mesh* m2 = new Mesh(data);
     print(m2);
@@ -114,18 +112,15 @@ struct MeshTest : Test {
           segs.front().size != buf->size() - (2*24*12))
         ctorChk = false;
     }
-    if (m2->impl().primitiveCount() != 1)
+    if (m2->primitiveCount() != 1)
       countChk = false;
-    if (m2->impl().topology(0) != CG_NS::TopologyTriangle)
+    if (m2->primitive(0).topology() != CG_NS::TopologyTriangle)
       topChk = false;
-    if (!m2->impl().canBind(VxTypePosition) ||
-        m2->impl().canBind(VxTypeJoints0))
+    if (!(m2->primitive(0).dataMask() & VxDataPosition) ||
+        m2->primitive(0).dataMask() & VxDataJoints0)
       bindChk = false;
 
-    prim.ixAccessor.dataIndex = 0;
-    prim.ixAccessor.dataOffset = 1024;
-    prim.ixAccessor.elementN = 36;
-    prim.ixAccessor.elementSize = 2;
+    prim.accessors.push_back({VxDataIndices, 0, 1024, 36, 2});
 
     Mesh m3(data);
     print(&m3);
@@ -133,15 +128,13 @@ struct MeshTest : Test {
     if (segs.size() != 1 || segs.front().offset != (3*24*12+36*2) ||
         segs.front().size != buf->size() - (3*24*12+36*2))
       ctorChk = false;
-    if (m3.impl().primitiveCount() != 1)
+    if (m3.primitiveCount() != 1)
       countChk = false;
-    if (m3.impl().topology(0) != CG_NS::TopologyTriangle)
+    if (m3[0].topology() != CG_NS::TopologyTriangle)
       topChk = false;
-    if (!m3.impl().canBind(VxTypePosition) ||
-        m3.impl().canBind(VxTypeTexCoord0))
+    if (!(m3[0].dataMask() & VxDataPosition) ||
+        m3[0].dataMask() & VxDataTexCoord0)
       bindChk = false;
-    if (!m3.impl().isIndexed())
-      ibufChk = false;
 
     delete m2;
     print();
@@ -151,7 +144,7 @@ struct MeshTest : Test {
         (++segs.begin())->size != buf->size() - (3*24*12+36*2))
       dtorChk = false;
 
-    prim.ixAccessor = {};
+    prim.accessors.pop_back();
 
     Mesh m4(data);
     print(&m4);
@@ -159,18 +152,15 @@ struct MeshTest : Test {
     if (segs.size() != 1 || segs.front().offset != (3*24*12+36*2) ||
         segs.front().size != buf->size() - (3*24*12+36*2))
       ctorChk = false;
-    if (m4.impl().primitiveCount() != 1)
+    if (m4.primitiveCount() != 1)
       countChk = false;
-    if (m4.impl().topology(0) != CG_NS::TopologyTriangle)
+    if (m4[0].topology() != CG_NS::TopologyTriangle)
       topChk = false;
-    if (!m4.impl().canBind(VxTypePosition) ||
-        m4.impl().canBind(VxTypeTexCoord1))
+    if (!(m4[0].dataMask() & VxDataPosition) ||
+        m4[0].dataMask() & VxDataTexCoord1)
       bindChk = false;
-    if (m4.impl().isIndexed())
-      ibufChk = false;
 
-    prim.vxAccessors.emplace(VxTypeTexCoord0,
-                             Mesh::Data::Accessor{0, 24*12, 24, 8});
+    prim.accessors.push_back({VxDataTexCoord0, 0, 24*12, 24, 8});
     prim.topology = CG_NS::TopologyPoint;
 
     Mesh m5(data);
@@ -179,21 +169,20 @@ struct MeshTest : Test {
     if (segs.size() != 1 || segs.front().offset != (4*24*12+36*2+24*8) ||
         segs.front().size != buf->size() - (4*24*12+36*2+24*8))
       ctorChk = false;
-    if (m5.impl().primitiveCount() != 1)
+    if (m5.primitiveCount() != 1)
       countChk = false;
-    if (m5.impl().topology(0) != CG_NS::TopologyPoint)
+    if (m5[0].topology() != CG_NS::TopologyPoint)
       topChk = false;
-    if (!m5.impl().canBind(VxTypePosition) ||
-        !m5.impl().canBind(VxTypeTexCoord0) ||
-        m5.impl().canBind(VxTypeTexCoord1))
+    if (!(m5[0].dataMask() & VxDataPosition) ||
+        !(m5[0].dataMask() & VxDataTexCoord0) ||
+        m5[0].dataMask() & VxDataTexCoord1)
       bindChk = false;
 
     a.push_back({L"Mesh(Data)", ctorChk});
     a.push_back({L"~Mesh()", dtorChk});
-    a.push_back({L"Mesh::Impl::primitiveCount()", countChk});
-    a.push_back({L"Mesh::Impl::topology()", topChk});
-    a.push_back({L"Mesh::Impl::canBind()", bindChk});
-    a.push_back({L"Mesh::Impl::isIndexed()", ibufChk});
+    a.push_back({L"primitiveCount()", countChk});
+    a.push_back({L"topology()", topChk});
+    a.push_back({L"dataMask()", bindChk});
 
     return a;
   }

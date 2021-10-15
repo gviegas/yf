9dd7506382df75fc5cd0cb2e872e618594888cf5
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-10-15 00:41:04 -0300

    [SG] ModelTest corrected for changes in Model

--- sg/test/ModelTest.cxx
+++ sg/test/ModelTest.cxx
@@ -10,7 +10,6 @@
 #include "Test.h"
 #include "Model.h"
 #include "Mesh.h"
-#include "Material.h"
 #include "Skin.h"
 
 using namespace TEST_NS;
@@ -35,49 +34,37 @@ struct ModelTest : Test {
                              mdl2.mesh() == &mesh && !mdl2.skin()});
 
     Skin skin(1, {});
-    Material material;
-    Model mdl3{mesh, skin, material};
+    Model mdl3{mesh, skin};
 
-    a.push_back({L"Model(mesh, skin, material)", mdl3.mesh() == &mesh &&
-                                                 mdl3.skin() == &skin &&
-                                                 mdl3.material() == &material});
+    a.push_back({L"Model(mesh, skinl)", mdl3.mesh() == &mesh &&
+                                        mdl3.skin() == &skin});
 
     Model mdl4{mdl3};
     a.push_back({L"Model(other)", mdl4.mesh() == &mesh &&
                                   mdl4.skin() == &skin &&
-                                  mdl4.material() == &material &&
                                   mdl4.mesh() == mdl3.mesh() &&
-                                  mdl4.skin() == mdl3.skin() &&
-                                  mdl4.material() == mdl3.material()});
+                                  mdl4.skin() == mdl3.skin()});
 
     Model mdl5 = mdl3;
     a.push_back({L"=", mdl5.mesh() == &mesh &&
                        mdl5.skin() == &skin &&
-                       mdl5.material() == &material &&
                        mdl5.mesh() == mdl3.mesh() &&
-                       mdl5.skin() == mdl3.skin() &&
-                       mdl5.material() == mdl3.material()});
+                       mdl5.skin() == mdl3.skin()});
 
     mdl1.setMesh(&mesh);
     mdl1.setSkin(&skin);
-    mdl1.setMaterial(&material);
 
-    mdl2.setMaterial(nullptr);
     mdl3.setMesh(nullptr);
     mdl4.setSkin(nullptr);
 
     a.push_back({L"set*()", mdl1.mesh() == &mesh &&
                             mdl1.skin() == &skin &&
-                            mdl1.material() == &material &&
                             mdl2.mesh() == &mesh &&
                             !mdl2.skin() &&
-                            !mdl2.material() &&
                             !mdl3.mesh() &&
                             mdl3.skin() == &skin &&
-                            mdl3.material() == &material &&
                             mdl4.mesh() == &mesh &&
-                            !mdl4.skin() &&
-                            mdl4.material() == &material});
+                            !mdl4.skin()});
 
     return a;
   }

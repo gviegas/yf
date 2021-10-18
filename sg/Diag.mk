cc45b535b4afe6e634f127afca10e7d360e2a6b3
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-10-18 15:17:10 -0300

    [SG] 'Diag.mk'

---
+++ sg/Diag.mk
@@ -0,0 +1,72 @@
+#!/usr/bin/env -S make -f
+
+#
+# SG
+# Diag.mk
+#
+# Copyright © 2021 Gustavo C. Viegas.
+#
+
+SHELL := /bin/sh
+.SUFFIXES: .cxx .o .d
+
+BASE_DIR := ../inc/
+INCLUDE_DIR := include/
+SRC_DIR := src/
+ETC_DIR := etc/
+SUB_DIR := sub/
+BUILD_DIR := build/
+
+SRC := \
+	$(wildcard $(SRC_DIR)*.cxx) \
+	$(wildcard $(ETC_DIR)*.cxx) \
+	$(wildcard $(SUB_DIR)*.cxx)
+
+OBJ := $(subst $(SRC_DIR),$(BUILD_DIR),$(SRC:.cxx=.o))
+OBJ := $(subst $(ETC_DIR),$(BUILD_DIR),$(OBJ))
+OBJ := $(subst $(SUB_DIR),$(BUILD_DIR),$(OBJ))
+
+DEP := $(OBJ:.o=.d)
+
+CXX := /usr/bin/c++
+CXX_FLAGS := -std=gnu++17 -Wpedantic -Wall -Wextra -g -fanalyzer
+
+LD_LIBS := -lyf-ws -lyf-cg
+LD_FLAGS := -iquote $(BASE_DIR) -iquote $(INCLUDE_DIR) -iquote $(SRC_DIR)
+
+PP := $(CXX) -E
+PP_FLAGS := -D YF_SG -D YF_DEVEL
+
+diag: $(OBJ)
+	@echo Done.
+
+-include $(DEP)
+
+.PHONY: clean-obj
+clean-obj:
+	rm -f $(OBJ)
+
+.PHONY: clean-dep
+clean-dep:
+	rm -f $(DEP)
+
+.PHONY: clean
+clean: clean-obj clean-dep
+
+$(BUILD_DIR)%.o: $(SRC_DIR)%.cxx
+	$(CXX) $(CXX_FLAGS) $(LD_FLAGS) $(PP_FLAGS) -c $< -o $@
+
+$(BUILD_DIR)%.o: $(ETC_DIR)%.cxx
+	$(CXX) $(CXX_FLAGS) $(LD_FLAGS) $(PP_FLAGS) -c $< -o $@
+
+$(BUILD_DIR)%.o: $(SUB_DIR)%.cxx
+	$(CXX) $(CXX_FLAGS) $(LD_FLAGS) $(PP_FLAGS) -c $< -o $@
+
+$(BUILD_DIR)%.d: $(SRC_DIR)%.cxx
+	@$(PP) $(LD_FLAGS) $(PP_FLAGS) $< -MM -MT $(@:.d=.o) > $@
+
+$(BUILD_DIR)%.d: $(ETC_DIR)%.cxx
+	@$(PP) $(LD_FLAGS) $(PP_FLAGS) $< -MM -MT $(@:.d=.o) > $@
+
+$(BUILD_DIR)%.d: $(SUB_DIR)%.cxx
+	@$(PP) $(LD_FLAGS) $(PP_FLAGS) $< -MM -MT $(@:.d=.o) > $@

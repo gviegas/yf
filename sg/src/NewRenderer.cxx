550cb28591ff5a3ebdadbb4b3840b1405ccee2d5
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-11-11 12:39:11 -0300

    NewRenderer ctor

--- sg/src/NewRenderer.cxx
+++ sg/src/NewRenderer.cxx
@@ -5,6 +5,8 @@
 // Copyright © 2021 Gustavo C. Viegas.
 //
 
+#include "yf/cg/Device.h"
+
 #include "NewRenderer.h"
 #include "Node.h"
 #include "Scene.h"
@@ -15,6 +17,19 @@
 using namespace SG_NS;
 using namespace std;
 
-NewRenderer::NewRenderer() { /* TODO */ }
+constexpr uint64_t UnifBufferSize = 1 << 21;
+
+constexpr CG_NS::DcEntry GlobalUnif{0, CG_NS::DcTypeUniform, 1};
+
+NewRenderer::NewRenderer() {
+  auto& dev = CG_NS::device();
+
+  cmdBuffer_ = dev.defaultQueue().cmdBuffer();
+  unifBuffer_ = dev.buffer(UnifBufferSize);
+
+  // This table will contain data common to all drawables
+  mainTable_ = dev.dcTable({GlobalUnif});
+  mainTable_->allocate(1);
+}
 
 void NewRenderer::processGraph(Node& rootNode) { /* TODO */ }

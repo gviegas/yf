eb0e3117f0ff0f7106418de7ace39bb62c7b8b04
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-11-24 14:44:34 -0300

    Changed PassVK ctor

--- cg/src/vk/PassVK.cxx
+++ cg/src/vk/PassVK.cxx
@@ -25,115 +25,41 @@ PassVK::PassVK(const vector<AttachDesc>* colors,
   if (colors && colors->size() > lim.maxColorAttachments)
     throw invalid_argument("Too many color attachments");
 
-  vector<VkAttachmentDescription> attachDescs;
-  vector<VkAttachmentReference> attachRefs;
-
-  // Define attachments
-  if (colors) {
-    auto add = [&](const vector<AttachDesc>& attachs) {
-      for (const auto& attach : attachs) {
-        if (aspectOfVK(attach.format) != VK_IMAGE_ASPECT_COLOR_BIT)
+  try {
+    if (colors) {
+      for (const auto& color : *colors) {
+        if (aspectOfVK(color.format) != VK_IMAGE_ASPECT_COLOR_BIT)
           throw invalid_argument("Invalid format for color attachment");
-
-        attachDescs.push_back({});
-
-        auto& desc = attachDescs.back();
-        desc.flags = 0;
-        desc.format = toFormatVK(attach.format);
-        desc.samples = toSampleCountVK(attach.samples);
-        desc.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
-        desc.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-        desc.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-        desc.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-        // FIXME: Won't work if an image view is used in more than one pass
-        desc.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-        desc.finalLayout = VK_IMAGE_LAYOUT_GENERAL;
-
-        attachRefs.push_back({static_cast<uint32_t>(attachDescs.size()-1),
-                              VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL});
       }
-    };
+      colors_ = new auto(*colors);
 
-    colors_ = new auto(*colors);
-    add(*colors);
+      if (resolves) {
+        if (colors->size() != resolves->size())
+          throw invalid_argument("Pass color/resolve attachment mismatch");
 
-    if (resolves) {
-      if (colors->size() != resolves->size())
-        throw invalid_argument("Pass color/resolve attachment mismatch");
-
-      resolves_ = new auto(*resolves);
-      add(*resolves);
+        for (size_t i = 0; i < colors->size(); i++) {
+          if ((*colors)[i].format != (*resolves)[i].format)
+            throw invalid_argument("Pass color/resolve attachment mismatch");
+        }
+        resolves_ = new auto(*resolves);
+      }
+    } else if (resolves) {
+      throw invalid_argument("Pass color/resolve attachment mismatch");
     }
+
+    if (depthStencil) {
+      auto aspect = aspectOfVK(depthStencil->format);
+      if (!(aspect & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)))
+        throw invalid_argument("Invalid format for depth/stencil attachment");
+
+      depthStencil_ = new auto(*depthStencil);
+    }
+  } catch (const bad_alloc&) {
+    delete colors_;
+    delete resolves_;
+    delete depthStencil_;
+    throw;
   }
-
-  if (depthStencil) {
-    auto aspect = aspectOfVK(depthStencil->format);
-    if (!(aspect & (VK_IMAGE_ASPECT_DEPTH_BIT | VK_IMAGE_ASPECT_STENCIL_BIT)))
-      throw invalid_argument("Invalid format for depth/stencil attachment");
-
-    depthStencil_ = new auto(*depthStencil);
-
-    attachDescs.push_back({});
-
-    auto& desc = attachDescs.back();
-    desc.flags = 0;
-    desc.format = toFormatVK(depthStencil->format);
-    desc.samples = toSampleCountVK(depthStencil->samples);
-    desc.loadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
-    desc.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-    desc.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_LOAD;
-    desc.stencilStoreOp = VK_ATTACHMENT_STORE_OP_STORE;
-    // FIXME: Won't work if an image view is used in more than one pass
-    desc.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-    desc.finalLayout = VK_IMAGE_LAYOUT_GENERAL;
-
-    attachRefs.push_back({static_cast<uint32_t>(attachDescs.size()-1),
-                          VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL});
-  }
-
-  // Define subpass
-  VkSubpassDescription subDesc;
-  subDesc.flags = 0;
-  subDesc.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-  subDesc.inputAttachmentCount = 0;
-  subDesc.pInputAttachments = nullptr;
-  subDesc.preserveAttachmentCount = 0;
-  subDesc.pPreserveAttachments = nullptr;
-
-  if (colors) {
-    subDesc.colorAttachmentCount = colors->size();
-    subDesc.pColorAttachments = attachRefs.data();
-    if (resolves)
-      subDesc.pResolveAttachments = attachRefs.data()+colors->size();
-    else
-      subDesc.pResolveAttachments = nullptr;
-  } else {
-    subDesc.colorAttachmentCount = 0;
-    subDesc.pColorAttachments = nullptr;
-    subDesc.pResolveAttachments = nullptr;
-  }
-
-  if (depthStencil)
-    subDesc.pDepthStencilAttachment = attachRefs.data()+attachRefs.size()-1;
-  else
-    subDesc.pDepthStencilAttachment = nullptr;
-
-  // Create render pass
-  VkRenderPassCreateInfo info;
-  info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-  info.pNext = nullptr;
-  info.flags = 0;
-  info.attachmentCount = attachDescs.size();
-  info.pAttachments = attachDescs.data();
-  info.subpassCount = 1;
-  info.pSubpasses = &subDesc;
-  info.dependencyCount = 0;
-  info.pDependencies = nullptr;
-
-  auto dev = deviceVK().device();
-  auto res = vkCreateRenderPass(dev, &info, nullptr, &renderPass_);
-  if (res != VK_SUCCESS)
-    throw DeviceExcept("Could not create render pass");
 }
 
 PassVK::~PassVK() {

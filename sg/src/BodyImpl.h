ffa9ed0da1d7519edab55929c909737728d58fd5
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2021-12-03 12:26:35 -0300

    Moved Body::Impl decl. to private header

---
+++ sg/src/BodyImpl.h
@@ -0,0 +1,41 @@
+//
+// SG
+// BodyImpl.h
+//
+// Copyright © 2021 Gustavo C. Viegas.
+//
+
+#ifndef YF_SG_BODYIMPL_H
+#define YF_SG_BODYIMPL_H
+
+#include <vector>
+
+#include "Body.h"
+
+SG_NS_BEGIN
+
+/// Physics Body implementation details.
+///
+class Body::Impl {
+ public:
+  Impl(const Shape& shape);
+  Impl(const std::vector<Shape*>& shapes);
+
+  void setNode(Node* node);
+  Node* node();
+
+ private:
+  std::vector<Sphere> spheres_{};
+  std::vector<BBox> bboxes_{};
+  Node* node_ = nullptr;
+  Vec3f localT_{};
+
+  void pushShape(const Shape& shape);
+  void next();
+  void undo();
+  bool check(Impl& other);
+};
+
+SG_NS_END
+
+#endif // YF_SG_BODYIMPL_H

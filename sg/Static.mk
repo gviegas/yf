a0dd5f5bc4b40abab6f309c99782a31fb24df406
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-06-16 18:06:33 -0300

    Add sg/Static.mk to make static library

---
+++ sg/Static.mk
@@ -0,0 +1,67 @@
+#!/usr/bin/env -S make -f
+
+SHELL := /bin/sh
+
+.SUFFIXES: .cxx .o .d
+
+VAR_DIR := ~/var/
+INCLUDE_DIR := include/
+SRC_DIR := src/
+CACHE_DIR := $(VAR_DIR)cache/yf/sg/
+LIB_DIR := $(VAR_DIR)
+LIB_BIN_DIR := $(LIB_DIR)lib/
+LIB_INC_DIR := $(LIB_DIR)include/yf/sg/
+
+LIB_A := $(LIB_BIN_DIR)libyf-sg.a
+
+SRC := $(wildcard $(SRC_DIR)*.cxx)
+
+OBJ := $(subst $(SRC_DIR),$(CACHE_DIR),$(SRC:.cxx=.o))
+
+DEP := $(OBJ:.o=.d)
+
+CC := /usr/bin/c++
+CC_FLAGS := -std=gnu++17 -Wpedantic -Wall -Wextra -O3
+
+LD_LIBS := -lm -lyf-ws -lyf-cg
+LD_FLAGS := -I $(VAR_DIR)include/ \
+	    -iquote $(INCLUDE_DIR) \
+	    -iquote $(SRC_DIR) \
+	    -L $(LIB_BIN_DIR)
+
+PP := $(CC) -E
+PP_FLAGS := -D YF -D YF_SG
+
+lib: $(LIB_A)
+	mkdir -pv $(LIB_INC_DIR)
+	cp -v $(INCLUDE_DIR)*.h $(LIB_INC_DIR)
+
+$(LIB_A): $(OBJ)
+	ar rcs $@ $^
+
+compile: $(OBJ)
+	@echo Done.
+
+.PHONY: clean-obj
+clean-obj:
+	rm -fv $(OBJ)
+
+.PHONY: clean-dep
+clean-dep:
+	rm -fv $(DEP)
+
+.PHONY: clean-lib
+clean-lib:
+	rm -fv $(LIB_A) $(LIB_INC_DIR)*.h
+	rmdir -v --ignore-fail-on-non-empty $(LIB_INC_DIR)
+
+.PHONY: clean
+clean: clean-obj clean-dep clean-lib
+
+$(CACHE_DIR)%.o: $(SRC_DIR)%.cxx
+	$(CC) $(CC_FLAGS) $(LD_FLAGS) $(PP_FLAGS) -c $< -o $@
+
+$(CACHE_DIR)%.d: $(SRC_DIR)%.cxx
+	@$(PP) $(LD_FLAGS) $(PP_FLAGS) $< -MM -MT $(@:.d=.o) > $@
+
+-include $(DEP)

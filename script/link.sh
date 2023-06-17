d3d04a3a6acf15dda961af8c4b7ccfacd4117813
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-06-17 11:42:22 -0300

    Update script/link.sh to create subdirectories' symlinks

--- script/link.sh
+++ script/link.sh
@@ -11,10 +11,14 @@ SG_DIR=sg
 # cg
 ln -srv test/Test.cxx $CG_DIR/test/Test.cxx
 ln -srv test/Test.h $CG_DIR/test/Test.h
+# TODO: Change this and .mk files to handle multiple subdirectories
+ln -srvT $CG_DIR/src/vk $CG_DIR/sub
 
 # ws
 ln -srv test/Test.cxx $WS_DIR/test/Test.cxx
 ln -srv test/Test.h $WS_DIR/test/Test.h
+# TODO: Change this and .mk files to handle multiple subdirectories
+ln -srvT $WS_DIR/src/unix $WS_DIR/sub
 
 # sg
 ln -srv test/Test.cxx $SG_DIR/test/Test.cxx

04199db3da49ea9dea3970c6d1cf25a816f186d5
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-06-22 16:52:44 -0300

    Make size/offset/range types generic & define new aliases

--- cg/include/Types.h
+++ cg/include/Types.h
@@ -14,111 +14,147 @@
 
 CG_NS_BEGIN
 
+namespace detail {
+  /// Two-dimensional size.
+  ///
+  template<class T>
+  struct Size2 {
+    T width;
+    T height;
+
+    Size2(T width, T height) : width(width), height(height) { }
+
+    bool operator==(const Size2& other) const {
+      return width == other.width && height == other.height;
+    }
+
+    bool operator!=(const Size2& other) const {
+      return !operator==(other);
+    }
+  };
+
+  /// Three-dimensional size.
+  ///
+  template<class T>
+  struct Size3 : Size2<T> {
+    T depthOrLayers;
+
+    Size3(T width, T height, T depthOrLayers)
+      : Size2<T>(width, height), depthOrLayers(depthOrLayers) { }
+
+    Size3(Size2<T> size2, T depthOrLayers)
+      : Size2<T>(size2), depthOrLayers(depthOrLayers) { }
+
+    bool operator==(const Size3& other) const {
+      return Size2<T>::operator==(other) &&
+             depthOrLayers == other.depthOrLayers;
+    }
+
+    bool operator!=(const Size3& other) const {
+      return !operator==(other);
+    }
+  };
+
+  /// Two-dimensional offset.
+  ///
+  template<class T>
+  struct Offset2 {
+    T x;
+    T y;
+
+    Offset2(T x, T y) : x(x), y(y) { }
+    Offset2() : Offset2({}, {}) { }
+
+    bool operator==(const Offset2& other) const {
+      return x == other.x && y == other.y;
+    }
+
+    bool operator!=(const Offset2& other) const {
+      return !operator==(other);
+    }
+  };
+
+  /// Three-dimensional offset.
+  ///
+  template<class T>
+  struct Offset3 : Offset2<T> {
+    T z;
+
+    Offset3(T x, T y, T z) : Offset2<T>(x, y), z(z) { }
+    Offset3(Offset2<T> offset2, T z) : Offset2<T>(offset2), z(z) { }
+    Offset3() : Offset3({}, {}, {}) { }
+
+    bool operator==(const Offset3& other) const {
+      return Offset2<T>::operator==(other) && z == other.z;
+    }
+
+    bool operator!=(const Offset3& other) const {
+      return !operator==(other);
+    }
+  };
+
+  /// Range.
+  ///
+  template<class T>
+  struct Range {
+    T start; // Inclusive
+    T end; // Exclusive
+
+    Range(T start, T end) : start(start), end(end) { }
+
+    bool operator==(const Range& other) const {
+      return start == other.start && end == other.end;
+    }
+
+    bool operator!=(const Range& other) const {
+      return !operator==(other);
+    }
+
+    /// Computes `end - start`.
+    ///
+    /// It assumes that `start` is no greater than `end`.
+    ///
+    T count() const {
+      return end - start;
+    }
+
+    /// Checks whether the range contains `other`.
+    ///
+    /// It assumes that `start` is no greater than `end`.
+    ///
+    bool contains(const Range& other) const {
+      return start <= other.start && end >= other.end;
+    }
+  };
+} // namespace detail
+
 /// Two-dimensional size.
 ///
-struct Size2 {
-  uint32_t width;
-  uint32_t height;
-
-  Size2(uint32_t width, uint32_t height) : width(width), height(height) { }
-
-  bool operator==(const Size2& other) const {
-    return width == other.width && height == other.height;
-  }
-
-  bool operator!=(const Size2& other) const {
-    return !operator==(other);
-  }
-};
+using Size2 = detail::Size2<uint32_t>;
 
 /// Three-dimensional size.
 ///
-struct Size3 : Size2 {
-  uint32_t depthOrLayers;
+using Size3 = detail::Size3<uint32_t>;
 
-  Size3(uint32_t width, uint32_t height, uint32_t depthOrLayers)
-    : Size2(width, height), depthOrLayers(depthOrLayers) { }
-
-  Size3(Size2 size2, uint32_t depthOrLayers)
-    : Size2(size2), depthOrLayers(depthOrLayers) { }
-
-  bool operator==(const Size3& other) const {
-    return Size2::operator==(other) && depthOrLayers == other.depthOrLayers;
-  }
-
-  bool operator!=(const Size3& other) const {
-    return !operator==(other);
-  }
-};
-
-/// Two-dimensional offset.
+/// Signed two-dimensional offset.
 ///
-struct Offset2 {
-  int32_t x;
-  int32_t y;
+using Offset2 = detail::Offset2<int32_t>;
 
-  Offset2(int32_t x, int32_t y) : x(x), y(y) { }
-  Offset2() : Offset2(0, 0) { }
-
-  bool operator==(const Offset2& other) const {
-    return x == other.x && y == other.y;
-  }
-
-  bool operator!=(const Offset2& other) const {
-    return !operator==(other);
-  }
-};
-
-/// Three-dimensional offset.
+/// Signed three-dimensional offset.
 ///
-struct Offset3 : Offset2 {
-  int32_t z;
+using Offset3 = detail::Offset3<int32_t>;
 
-  Offset3(int32_t x, int32_t y, int32_t z) : Offset2(x, y), z(z) { }
-  Offset3(Offset2 offset2, int32_t z) : Offset2(offset2), z(z) { }
-  Offset3() : Offset3(0, 0, 0) { }
+/// Unsigned two-dimensional origin.
+///
+using Origin2 = detail::Offset2<uint32_t>;
 
-  bool operator==(const Offset3& other) const {
-    return Offset2::operator==(other) && z == other.z;
-  }
-
-  bool operator!=(const Offset3& other) const {
-    return !operator==(other);
-  }
-};
+/// Unsigned three-dimensional origin.
+///
+using Origin3 = detail::Offset3<uint32_t>;
 
 /// Range.
 ///
-struct Range {
-  uint32_t start; // Inclusive
-  uint32_t end; // Exclusive
-
-  Range(uint32_t start, uint32_t end) : start(start), end(end) { }
-
-  bool operator==(const Range& other) const {
-    return start == other.start && end == other.end;
-  }
-
-  bool operator!=(const Range& other) const {
-    return !operator==(other);
-  }
-
-  /// Computes `end - start`.
-  ///
-  /// It assumes that `start` is no greater than `end`.
-  ///
-  uint32_t count() const {
-    return end - start;
-  }
-
-  /// Checks whether the range contains `other`.
-  ///
-  /// It assumes that `start` is no greater than `end`.
-  ///
-  bool contains(const Range& other) const {
-    return start <= other.start && end >= other.end;
-  }
-};
+using Range = detail::Range<uint32_t>;
 
 CG_NS_END
 

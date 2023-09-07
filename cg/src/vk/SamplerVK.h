465f761b343f47916d878080649507bf0a459b15
Gustavo C. Viegas <gviegas@users.noreply.github.com>
2023-09-07 17:45:00 -0300

    [CG] Add toCompareOpVK conversion function

--- cg/src/vk/SamplerVK.h
+++ cg/src/vk/SamplerVK.h
@@ -62,7 +62,24 @@ inline VkSamplerMipmapMode toMipmapModeVK(MipFilter mipFilter) {
   }
 }
 
-// TODO: `CmpFunc` conversion.
+/// Converts from a `CmpFunc` value.
+///
+inline VkCompareOp toCompareOpVK(CmpFunc cmpFunc) {
+  switch (cmpFunc) {
+  case CmpFunc::Never:        return VK_COMPARE_OP_NEVER;
+  case CmpFunc::Less:         return VK_COMPARE_OP_LESS;
+  case CmpFunc::Equal:        return VK_COMPARE_OP_EQUAL;
+  case CmpFunc::LessEqual:    return VK_COMPARE_OP_LESS_OR_EQUAL;
+  case CmpFunc::Greater:      return VK_COMPARE_OP_GREATER;
+  case CmpFunc::NotEqual:     return VK_COMPARE_OP_NOT_EQUAL;
+  case CmpFunc::GreaterEqual: return VK_COMPARE_OP_GREATER_OR_EQUAL;
+  case CmpFunc::Always:       return VK_COMPARE_OP_ALWAYS;
+
+  case CmpFunc::None:
+  default:
+    throw std::invalid_argument(__func__);
+  }
+}
 
 CG_NS_END
 
